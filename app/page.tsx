97a22a86f63750473539631264b9cf9199215fcc
Rafaela Ferro <rferro@memsql.com>
2024-03-23 14:25:04 +0000

    Add text animation component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
-import { museoModerno } from "@/app/fonts";
 import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
+import { TextAnimation } from "@/components/animations/text-animation";
 
 export default function Home() {
     return (
@@ -18,21 +17,9 @@ export default function Home() {
                     />
                 </div>
             </div>
-            <div className="flex flex-grow flex-auto text-center items-center justify-center w-full">
-                <h1 className={cn(museoModerno.className, "flex flex-row gap-2")}>
-                    <span className="text-4xl md:text-7xl font-black">Blockchain</span>
-                    <span className="text-4xl md:text-7xl font-extralight">made</span>
-                    <span className="relative w-[100px] md:w-[200px]  h-auto">
-                        <Image
-                            src="/simpl3.svg"
-                            alt="simpl3 Logo"
-                            fill={true}
-                            className="md:mt-1"
-                        />
-                    </span>
 
-                </h1>
-            </div>
+            <TextAnimation />
+
             <div className="flex items-center justify-center py-12 bg-muted w-full">
                 <div className="mx-auto grid w-[450px] gap-6">
 
