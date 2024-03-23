067070eb3ad449c6b2d41f38c9d24e658fed708d
0xDistro <hey@0xDistro.com>
2024-03-23 00:55:51 +0000

    Adds ui and homepage

---
+++ app/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, MuseoModerno } from 'next/font/google'
+
+export const inter = Inter({
+    subsets: ['latin'],
+    display: 'swap',
+})
+
+export const museoModerno = MuseoModerno({
+    subsets: ['latin'],
+    display: 'swap',
+})
